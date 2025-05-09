8f8e7e004774c5f43e4238255e702701a8caf6c7
Daniel <daniel.bintar@gmail.com>
2025-05-09 16:45:49 +0800

    example: demo simple

---
+++ demo-1-simple/backend/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/api/v1/hello", helloHandler)
+	http.HandleFunc("/api/v3/hello", helloHandler)
+
+	port := ":8080"
+	log.Printf("listen %s", port)
+	http.ListenAndServe(port, nil)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{"message": "Hello, world!"})
+}
