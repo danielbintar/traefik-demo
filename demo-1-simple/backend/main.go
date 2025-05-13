5cda60448d0dccba3b26ea418fb60e8b64f66864
Daniel Bintar Doharto Sagala Raja Sijabat <daniel.sijabat@shopee.com>
2025-05-13 17:15:55 +0800

    feat: return path on backend to differentiate the target

--- demo-1-simple/backend/main.go
+++ demo-1-simple/backend/main.go
@@ -17,5 +17,5 @@ func main() {
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(map[string]string{"message": "Hello, world!"})
+	json.NewEncoder(w).Encode(map[string]string{"message": "Hello, world!", "path": r.URL.Path})
 }
